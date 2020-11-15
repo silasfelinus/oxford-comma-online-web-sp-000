b10ddd1edb9ae80bea3adb431f3d6ed6dc78869c
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-15 01:04:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 
   if array.length == 1
     array[0]
-  elsif array == 2
+  elsif array.length == 2
     array[0] + " and " + array[1]
   else
 "#{array[0...-1].join(", ")}, and #{array[-1]}"
